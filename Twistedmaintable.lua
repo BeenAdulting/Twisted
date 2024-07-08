33bacbc382c5e028ec167faf92e973dff2b4a350
Adulting <157926852+BeenAdulting@users.noreply.github.com>
2024-07-08 01:29:28 -0500

    Update Twistedmaintable.lua

--- Twistedmaintable.lua
+++ Twistedmaintable.lua
@@ -1,11 +1,1 @@
-local Twisted = {
-    ['Camlock'] = {
-        Keybind = Enum.KeyCode.C,
-        CameraPred = 0.39, 
-        Hitpart = true,
-        TargetPart = "HumanoidRootPart", 
-        Smoothing = 0.0612, 
-        Shake = 0,
-        JumpOffset = 1.50,
-    },
-}
+
