c1acf586ff0dc5764933742c43526c2f317f2093
Adulting <157926852+BeenAdulting@users.noreply.github.com>
2024-07-08 01:32:19 -0500

    Update Twistedmaintable.lua

--- Twistedmaintable.lua
+++ Twistedmaintable.lua
@@ -1,1 +1,38 @@
+local Twisted = {
+    ['Loader'] = {
+        ['Key'] = "", --// Key Here.
+        ['Intro'] = false,
+        ['FPSUnlocker'] = false,
+    },
+    ["Camlock"] = {
+    },
+    ['Silent Aim'] = {
 
+    },
+    ['Sorting'] = {
+        ['Enabled'] = false, 
+        ['Keybind'] = Enum.KeyCode.P,
+        ['Slot 1'] = "[Double-Barrel SG]",
+        ['Slot 2'] = "[Revolver]",
+        ['Slot 3'] = "[]",
+        ['Slot 4'] = "[]",
+        ['Slot 5'] = "[]",
+        ['Slot 6'] = "[]",
+        ['Slot 7'] = "[]",
+        ['Slot 8'] = "[]",
+        ['Slot 9'] = "[]",
+        ['Slot 0'] = "[]",
+    },
+    ['Resolver'] = {
+
+    },
+    ['Anti-Lock'] = {
+        ['Key'] = Enum.KeyCode.C, 
+        ['Enabled'] = false,
+    },
+    ['Misc'] = {
+        ['No Recoil'] = false, 
+    }
+}
+
+script_key = Twisted.Loader.Key
